3c4a4b029546fce9d998b37accefa2b7636d60ab
Sam <samuelchen4@gmail.com>
2020-12-07 12:30:04 -0600

    Created Product class and Product service

---
+++ 03-frontend/angular-ecommerce/src/app/common/product.ts
@@ -0,0 +1,12 @@
+export class Product {
+
+    sku: string;
+    name: string;
+    description: string;
+    unitPrice: string;
+    imageUrl: string;
+    active: string;
+    unitsInStock: string;
+    dateCreated: string;
+    lastUpdated: string;
+}
