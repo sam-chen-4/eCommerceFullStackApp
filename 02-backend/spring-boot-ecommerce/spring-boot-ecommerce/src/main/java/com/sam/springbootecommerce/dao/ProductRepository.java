4b4055fa1c60d57ff0dd5bd808815765bccde1b6
Sam <samuelchen4@gmail.com>
2020-12-07 13:14:09 -0600

    Added @CrossOrigin support to enable front-end calls to back-end

--- 02-backend/spring-boot-ecommerce/spring-boot-ecommerce/src/main/java/com/sam/springbootecommerce/dao/ProductRepository.java
+++ 02-backend/spring-boot-ecommerce/spring-boot-ecommerce/src/main/java/com/sam/springbootecommerce/dao/ProductRepository.java
@@ -3,6 +3,8 @@ package com.sam.springbootecommerce.dao;
 import com.sam.springbootecommerce.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+@CrossOrigin("http://localhost:4200")
 public interface ProductRepository extends JpaRepository<Product, Long> {
 }
